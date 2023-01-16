056a8caeb7db3b4caf83fa537e37dd7eabfc146e
Tec <Tec@SKAB1-PC4>
2023-01-16 13:17:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ctof
+{
+    internal class Opgaveyk
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skrive hvor mange Celcius du ville convert til Fahrenheit");
+            Console.Write("C: ");
+            int ctof = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"F: {ctofconverter(ctof)} ");
+
+            Console.ReadKey();
+
+            static float ctofconverter(int ctof)
+            {
+                return ctof * 9 / 5 + 32;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
